b428f8205669ec09f6d7d675fa3e327d36152d6c
Aida Martin <aidamg93@gmail.com>
2023-07-19 14:30:30 +0200

    Pass Api handleErrors to async-await

--- staff/aida-martin/fullstack/api/handlers/helpers/handleErrors.js
+++ staff/aida-martin/fullstack/api/handlers/helpers/handleErrors.js
@@ -11,18 +11,24 @@ const {
 module.exports = (callback) => {
   return (req, res) => {
     try {
-      callback(req, res).catch((error) => {
-        let status = 500
+      const promise = callback(req, res)
 
-        if (error instanceof DuplicityError || error instanceof PropertyError)
-          status = 409
-        else if (error instanceof ExistenceError) status = 404
-        else if (error instanceof AuthError) status = 401
+      ;(async () => {
+        try {
+          await promise
+        } catch (error) {
+          let status = 500
 
-        res
-          .status(status)
-          .json({ message: error.message, type: error.constructor.name })
-      })
+          if (error instanceof DuplicityError || error instanceof PropertyError)
+            status = 409
+          else if (error instanceof ExistenceError) status = 404
+          else if (error instanceof AuthError) status = 401
+
+          res
+            .status(status)
+            .json({ message: error.message, type: error.constructor.name })
+        }
+      })()
     } catch (error) {
       let status = 500
 
