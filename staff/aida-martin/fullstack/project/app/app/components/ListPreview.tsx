63df5d0ac2607bcb3d3d7b489d8f9be824d9454e
Aida Martin <aidamg93@gmail.com>
2023-07-27 12:48:05 +0200

    Create ListPreview compo and render lists page

---
+++ staff/aida-martin/fullstack/project/app/app/components/ListPreview.tsx
@@ -0,0 +1,44 @@
+import Container from './Container'
+import Image from 'next/image'
+
+interface ListPreviewProps {
+  icon: JSX.Element
+  tittle: string
+  quantity: number
+  subtittle: string
+  image: string
+  color: string
+}
+
+export default function ListPreview({
+  icon,
+  tittle,
+  quantity,
+  subtittle,
+  image,
+  color,
+}: ListPreviewProps) {
+  return (
+    <Container className={`p-5 ${color}`}>
+      <h2 className="text-text-light text-xl font-semibold">{tittle}</h2>
+      <div className="grid grid-cols-2 place-items-center">
+        <div className="flex flex-col items-center p-10">
+          {icon}
+          <h3 className="text-text-light text-4xl font-bold">{quantity}</h3>
+        </div>
+        <div className="flex flex-col items-center p-2">
+          <h3 className="text-text-light text-lg font-normal">{subtittle}</h3>
+          <div>
+            <Image
+              src={image}
+              alt="Preview"
+              width="130"
+              height="130"
+              quality="100"
+            />
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
+}
--- staff/aida-martin/fullstack/project/app/app/lists/page.tsx
+++ staff/aida-martin/fullstack/project/app/app/lists/page.tsx
@@ -1,5 +1,37 @@
-export default function Lists () {
-    return (
-        <h1>Esto es la ruta Lists</h1>
-    )
+import Tittle from '../components/Tittle'
+import ListPreview from '../components/ListPreview'
+import ContainerLink from '../components/ContainerLink'
+import { lists } from '../infraestructure/lists'
+import { IconBookmarkFill, IconHeartFill } from '../components/Icons'
+
+export default function Lists() {
+  return (
+    <section className="p-4 bg-white">
+      <Tittle name="Your lists" />
+
+      <div className="flex flex-col gap-3">
+        <ContainerLink route={lists[0].route}>
+          <ListPreview
+            tittle="My collection"
+            icon={<IconBookmarkFill size="70px" />}
+            quantity={323}
+            subtittle="Last added"
+            image="/previews/Preview-Snow-White.webp"
+            color="text-[#007cec]"
+          ></ListPreview>
+        </ContainerLink>
+
+        <ContainerLink route={lists[1].route}>
+          <ListPreview
+            tittle="My whislist"
+            icon={<IconHeartFill size="70px" />}
+            quantity={102}
+            subtittle="Last added"
+            image="/previews/Preview-Peter-Pan.webp"
+            color="text-[#EC0063]"
+          ></ListPreview>
+        </ContainerLink>
+      </div>
+    </section>
+  )
 }
