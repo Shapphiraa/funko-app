cbab045b8e45d6112240cdbe4f1fb458d0525c7d
Aida Martin <aidamg93@gmail.com>
2023-09-04 14:58:46 +0200

    Change pops width on Products compo

--- staff/aida-martin/fullstack/project/app/app/components/Products.tsx
+++ staff/aida-martin/fullstack/project/app/app/components/Products.tsx
@@ -75,7 +75,7 @@ export default function Products({
 
       {!isLoading && pops && pops.length > 0 && (
         <div
-          className={`w-full max-w-6xl mx-auto grid grid-cols-[repeat(auto-fit,_minmax(136px,1fr))] gap-4 ${className}`}
+          className={`w-full max-w-6xl mx-auto grid grid-cols-2 gap-4 ${className}`}
         >
           {pops.map((pop) => (
             <Product pop={pop} key={pop.id} onChange={getPops} />
